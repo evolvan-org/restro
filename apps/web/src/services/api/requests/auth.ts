04ab4b609d97d7f1acabc939fb3c732713616fa1
Aryan <aryank@evolvan.com>
2026-09-23 15:44:39 +0530

    Add useLogin hook for login api call and set access token in redux store

--- apps/web/src/services/api/requests/auth.ts
+++ apps/web/src/services/api/requests/auth.ts
@@ -1,3 +1,24 @@
 import { useMutation } from '@tanstack/react-query';
-import { LoginRequest } from '../types/LoginRequest';
-import { LoginResponse } from '../types/LoginResponse';
+import { loginResponseSchema, type LoginRequest, type LoginResponse } from '@rms/api-contract';
+
+import { api } from '@/lib/api';
+import { actions } from '@/store/slices/auth';
+
+import useAppDispatch from '@/store/hooks/useAppDispatch';
+
+const useLogin = () => {
+  const dispatch = useAppDispatch();
+
+  return useMutation({
+    mutationFn: async (loginRequest: LoginRequest): Promise<LoginResponse> => {
+      const response = await api.post('/auth/login', loginRequest);
+
+      return loginResponseSchema.parse(response.data);
+    },
+    onSuccess: (response) => {
+      dispatch(actions.login({ accessToken: response.data.accessToken }));
+    },
+  });
+};
+
+export { useLogin };
